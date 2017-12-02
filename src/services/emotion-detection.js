26673e415537f052cd0ea2641e7feb7df8a59ab3
ashlinaronin <ashlinaronin@gmail.com>
2017-12-01 23:08:27 -0800

    Remove Kairos from emotion results since it was so useless.
    It's still good to have this flexible architecture in case we want to swap out SkyBiometry for something else later down the line.

--- src/services/emotion-detection.js
+++ src/services/emotion-detection.js
@@ -1,20 +1,14 @@
 import skyBioGetEmotions from 'services/sky-biometry';
-import kairosGetEmotions from 'services/kairos';
 
 export default async function getEmotions(imageUrl) {
   try {
     const skyBioPromise = skyBioGetEmotions(imageUrl);
-    const kairosPromise = kairosGetEmotions(imageUrl);
 
     return {
       resultsByProvider: [
         {
           provider: 'skyBiometry',
           results: await skyBioPromise,
-        },
-        {
-          provider: 'kairos',
-          results: await kairosPromise,
         },
       ],
       error: null,
