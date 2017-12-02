1f260543ec6fc5f82a0cecfd7bc5ccd2a5970fed
ashlinaronin <ashlinaronin@gmail.com>
2017-12-01 21:39:05 -0800

    Fix face chopping coordinates. Apparently Sky Biometry uses a 0-100 coordinate system?

--- backend/services/face-chopper.js
+++ backend/services/face-chopper.js
@@ -1,15 +1,18 @@
 const sharp = require('sharp');
+const fetch = require('isomorphic-fetch');
+
+const SMILE_WIDTH_TO_FACE_WIDTH_RATIO = 0.5;
+const SMILE_HEIGHT_TO_FACE_HEIGHT_RATIO = 0.2;
 
 async function saveSmilesFromDonation(donation) {
+  // SkyBiometry coordinates appear to be in the range of 0-100... wtf? We can work with it...
+  const smileWidth = (donation.faceWidth / 100) * donation.imageTotalWidth * SMILE_WIDTH_TO_FACE_WIDTH_RATIO;
+  const smileHeight = (donation.faceHeight / 100) * donation.imageTotalHeight * SMILE_HEIGHT_TO_FACE_HEIGHT_RATIO;
+  const smileLeftX = ((donation.mouthCenter.x / 100) * donation.imageTotalWidth) - (smileWidth / 2);
+  const smileTopY = ((donation.mouthCenter.y / 100) * donation.imageTotalHeight) - (smileHeight / 2);
 
-  const smileWidth = donation.faceWidth / 3;
-  const smileHeight = donation.faceHeight / 22;
-
-  const smileLeftX = donation.mouthCenter.x - (smileWidth / 2);
-  const smileTopY = donation.mouthCenter.y - (smileHeight / 2);
-
-  const input = sharp('./uploads/1510459238548filething.png');
-  // const input = sharp(donation.originalImageUrl);
+  const file = await fetchImage(donation.originalImageUrl);
+  const input = sharp(file);
   const output = `./uploads/smiles/${ donation._id }.png`;
 
   return input
@@ -21,8 +24,22 @@ async function saveSmilesFromDonation(donation) {
     })
     .toFile(output)
     .catch(err => {
-      console.log('Error extracting smile from image:', err);
+      console.log('FaceChopper: Error extracting smile from image:', err);
     });
+}
+
+async function fetchImage(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      return await response.buffer();
+    } else {
+      return Promise.reject('FaceChopper: Error fetching original donation image');
+    }
+  }
+  catch (err) {
+    console.log('FaceChopper: Error fetching original donation image');
+  }
 }
 
 module.exports = {
--- backend/services/sky-biometry-parser.js
+++ backend/services/sky-biometry-parser.js
@@ -6,28 +6,33 @@ function skyBioResponseToDonation(skyBioResponse) {
     rawResponse: skyBioResponse
   });
 
-  const firstPhoto = skyBioResponse.photos[0];
+  try {
+    const firstPhoto = skyBioResponse.photos[0];
 
-  if (firstPhoto.tags.length === 0) {
-    donation.facePresent = false;
-    return donation;
+    if (firstPhoto.tags.length === 0) {
+      donation.facePresent = false;
+      return donation;
+    }
+
+    const firstFace = firstPhoto.tags[0];
+    const attributes = firstFace.attributes;
+
+    donation.originalImageUrl = firstPhoto.url;
+    donation.gender = attributes.gender.value;
+    donation.age = attributes.age_est.value;
+    donation.mood = attributes.mood.value;
+    donation.isSmiling = attributes.smiling.value;
+
+    donation.faceWidth = firstFace.width;
+    donation.faceHeight = firstFace.height;
+    donation.imageTotalWidth = firstPhoto.width;
+    donation.imageTotalHeight = firstPhoto.height;
+    donation.mouthCenter.x = firstFace.mouth_center.x;
+    donation.mouthCenter.y = firstFace.mouth_center.y;
   }
-
-  const firstFace = firstPhoto.tags[0];
-  const attributes = firstFace.attributes;
-
-  donation.originalImageUrl = firstPhoto.url;
-  donation.gender = attributes.gender.value;
-  donation.age = attributes.age_est.value;
-  donation.mood = attributes.mood.value;
-  donation.isSmiling = attributes.smiling.value;
-
-  donation.faceWidth = firstFace.width;
-  donation.faceHeight = firstFace.height;
-  donation.imageTotalWidth = firstPhoto.width;
-  donation.imageTotalHeight = firstPhoto.height;
-  donation.mouthCenter.x = firstFace.mouth_center.x;
-  donation.mouthCenter.y = firstFace.mouth_center.y;
+  catch (err) {
+    console.log('Error parsing donation, swallowing');
+  }
 
   return donation;
 }
