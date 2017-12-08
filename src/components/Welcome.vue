6396dc44ae05f7539d0d62593901f2d52e7678f0
ashlinaronin <ashlinaronin@gmail.com>
2017-12-07 22:38:55 -0800

    Add link to donate page on welcome page. Still needs some styling...

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,6 +2,7 @@
   <div class="welcome">
     <h1>Welcome!</h1>
     <p>Welcome copy here. asdf jkasl dfjkla sdjfk lasjdkf aljsd fkla sjkfdl ajskdfl jsk f</p>
+    <router-link to="donate">I'm ready</router-link>
   </div>
 </template>
 
